7b0099afb5ac2fc422f396cce570235b148a1fc5
GlebV <glebvoronin03@yandx.ru>
2023-09-28 12:29:04 +0300

    пример обработки событий

--- ui_manage_households.py
+++ ui_manage_households.py
@@ -56,16 +56,36 @@ class Ui_Dialog(object):
         TABLE_ROW_LIMIT = 10
         vet_db_connection = create_engine(f'sqlite:///{DB_PATH}').connect()
 
-        #data_for_table=self.get_data_db(self, vet_db_connection, "city")
-        data_for_table=pd.read_sql(f'SELECT * FROM city', vet_db_connection)
-        self.citiesWidget.setColumnCount(3)
+        # data_for_table=self.get_data_db(self, vet_db_connection, "city")
+        # cities         = pd.read_sql(f'''SELECT * FROM city''', vet_db_connection).rename(columns={'name': 'city_name'})
+        # settlements    = pd.read_sql(f'''SELECT * FROM settlement''', vet_db_connection).rename(columns={'pk': 'pk_settlement', 'name': 'settlement_name'})
+        # data_for_table = cities.merge(settlements, left_on='belongs_to_settlement', right_on='pk_settlement', how='outer')
+        # data_for_table = data_for_table[['city_name', 'settlement_name']].fillna('')
+
+        data_for_table = pd.read_sql(f'''
+            SELECT city.name as CityName, settlement.name as SettlementName FROM city
+            INNER JOIN settlement
+                ON settlement.pk = city.belongs_to_settlement;
+        ''', vet_db_connection)
+
+        data_for_table = data_for_table.filter(set(data_for_table.columns) - {'belongs_to_settlement', 'pk'})
+        print(data_for_table)
+
+        self.citiesWidget.setColumnCount(len(data_for_table.columns))
         self.citiesWidget.setRowCount(len(data_for_table))
+        self.citiesWidget.setHorizontalHeaderLabels(data_for_table.columns)
 
-        for col_num in range(0,2):
-            for row_num in range(0,len(data_for_table)):
-                self.citiesWidget.setItem(row_num, col_num, QTableWidgetItem(data_for_table.loc[row_num, col_num]))
+        for col_num, col in enumerate(data_for_table.columns):
+            for row_num, row in enumerate(data_for_table.index):
+                self.citiesWidget.setItem(row_num, col_num, QTableWidgetItem(data_for_table.loc[row, col]))
 
+        def add_new_city(event, table, label):
+            if event.key() == Qt.Key.Key_Delete and table.currentColumn() == 0:
+                label.setText(table.currentItem().text())
 
+        self.citiesWidget.keyPressEvent = lambda x: add_new_city(x, self.citiesWidget, self.label)
+
+        QMetaObject.connectSlotsByName(Dialog)
 
         QMetaObject.connectSlotsByName(Dialog)
     # setupUi
