23162894f2a477a639f79bb12f091fa66e2999a5
yuriiudodov <udodov.ura2003@gmail.com>
2023-11-27 15:13:40 +0300

    не работает выбор

--- ui_dialog_open.py
+++ ui_dialog_open.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 ################################################################################
 ## Form generated from reading UI file 'dialog_open.ui'
@@ -23,17 +22,26 @@ from sqlalchemy import create_engine, text
 import ui_edit_cities
 import ui_edit_settlements
 
-
 class Ui_Dialog(object):
+    selected_table=0
+    def set_edit_method(self, editmethod):
+        selected=editmethod
+    def edit_button_pressed(self):
+        if(self.selected_table==0):
+            print("open zero")
+        if(self.selected_table==1):
+            print("open first")
+        if (self.selected_table == 2):
+            print("open second")
     def open_settlement_edit(self):
         self.window = QDialog()
         self.ui = ui_edit_settlements.Ui_Dialog()
         self.ui.setupUi(self.window)
         self.window.show()
 
-    def open_city_edit(self):#opens hyety nesysvetnyu
+    def open_city_edit(self):  # opens hyety nesysvetnyu
         self.window = QDialog()
-        self.ui=ui_edit_cities.Ui_Dialog()
+        self.ui = ui_edit_cities.Ui_Dialog()
         self.ui.setupUi(self.window)
         self.window.show()
 
@@ -65,22 +73,23 @@ class Ui_Dialog(object):
 
         pandas_SQL_query = f'SELECT household.pk, household.address, household.owner, city.name FROM household INNER JOIN city ON household.belongs_to_city = city.pk WHERE city.pk = {self.cityTableWidget.item(self.cityTableWidget.currentRow(), 0).text()}'
 
-        data_for_table = pd.read_sql(pandas_SQL_query,vet_db_connection).astype(str)
+        data_for_table = pd.read_sql(pandas_SQL_query, vet_db_connection).astype(str)
 
         self.householdTableWidget.setColumnCount(4)
         self.householdTableWidget.setRowCount(len(data_for_table))
 
         for col_num in range(len(data_for_table.columns)):
-            print("\n")#debug
+            print("\n")  # debug
             for row_num in range(0, len(data_for_table)):
-                print(QTableWidgetItem(data_for_table.iloc[row_num, col_num]).text())#debug
+                print(QTableWidgetItem(data_for_table.iloc[row_num, col_num]).text())  # debug
                 self.householdTableWidget.setItem(row_num, col_num,
                                                   QTableWidgetItem(data_for_table.iloc[row_num, col_num]))
+
     def refresh_table(self):
         DB_PATH = 'MainDatabaseVet'  # vremennoe reshenie
         TABLE_ROW_LIMIT = 10
         vet_db_connection = create_engine(f'sqlite:///{DB_PATH}').connect()
-        #-----------------settlements_table------------------------
+        # -----------------settlements_table------------------------
         data_for_table = pd.read_sql(text(f'SELECT pk,name FROM settlement'), vet_db_connection)
         self.settlementTableWidget.setColumnCount(2)
         self.settlementTableWidget.setRowCount(len(data_for_table))
@@ -90,7 +99,9 @@ class Ui_Dialog(object):
                 self.settlementTableWidget.setItem(row_num, col_num,
                                                    QTableWidgetItem(data_for_table.iloc[row_num, col_num]))
         # -----------------cities_table------------------------
-        data_for_table = pd.read_sql(text(f'SELECT city.pk, city.name, settlement.name FROM city LEFT JOIN settlement ON city.belongs_to_settlement = settlement.pk'), vet_db_connection)
+        data_for_table = pd.read_sql(text(
+            f'SELECT city.pk, city.name, settlement.name FROM city LEFT JOIN settlement ON city.belongs_to_settlement = settlement.pk'),
+                                     vet_db_connection)
         self.cityTableWidget.setColumnCount(3)
         self.cityTableWidget.setRowCount(len(data_for_table))
         display(data_for_table)
@@ -98,50 +109,77 @@ class Ui_Dialog(object):
         for col_num in range(len(data_for_table.columns)):
             for row_num in range(0, len(data_for_table)):
                 self.cityTableWidget.setItem(row_num, col_num,
-                                                   QTableWidgetItem(data_for_table.iloc[row_num, col_num]))
+                                             QTableWidgetItem(data_for_table.iloc[row_num, col_num]))
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(1062, 575)
 
-    def setupUi(self, Dialog):
-        if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
-        Dialog.resize(851, 621)
-        self.cityTableWidget = QTableWidget(Dialog)
-        self.cityTableWidget.setObjectName(u"cityTableWidget")
-        self.cityTableWidget.setGeometry(QRect(300, 70, 256, 192))
-        self.settlementTableWidget = QTableWidget(Dialog)
-        self.settlementTableWidget.itemClicked.connect(lambda: self.fill_cities_table())
-        self.settlementTableWidget.setObjectName(u"settlementTableWidget")
-        self.settlementTableWidget.setGeometry(QRect(10, 70, 256, 192))
 
-        self.label = QLabel(Dialog)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 50, 141, 16))
-        self.label_2 = QLabel(Dialog)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(300, 50, 141, 16))
-        self.settlementPushButton = QPushButton(Dialog, clicked=lambda: self.open_settlement_edit())
-        self.settlementPushButton.setObjectName(u"settlementPushButton")
-        self.settlementPushButton.setGeometry(QRect(20, 270, 161, 24))
-        self.cityPushButton = QPushButton(Dialog, clicked=lambda: self.open_city_edit())
-        self.cityPushButton.setObjectName(u"cityPushButton")
-        self.cityPushButton.setGeometry(QRect(300, 270, 161, 24))
-        self.householdTableWidget = QTableWidget(Dialog)
+        self.householdTableWidget = QTableWidget(Form)
+        if (self.householdTableWidget.columnCount() < 4):
+            self.householdTableWidget.setColumnCount(4)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.householdTableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.householdTableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.householdTableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.householdTableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
         self.householdTableWidget.setObjectName(u"householdTableWidget")
-        self.householdTableWidget.setGeometry(QRect(580, 70, 256, 192))
-        self.householdPushButton = QPushButton(Dialog, clicked=lambda: self.open_household_edit())
+
+        self.householdTableWidget.setGeometry(QRect(640, 40, 411, 471))
+        self.cityTableWidget = QTableWidget(Form)
         self.cityTableWidget.itemClicked.connect(lambda: self.fill_households_table())
-        self.householdPushButton.setObjectName(u"householdPushButton")
-        self.householdPushButton.setGeometry(QRect(580, 270, 161, 24))
-        self.label_3 = QLabel(Dialog)
+        if (self.cityTableWidget.columnCount() < 3):
+            self.cityTableWidget.setColumnCount(3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.cityTableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.cityTableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.cityTableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem6)
+        self.cityTableWidget.setObjectName(u"cityTableWidget")
+        self.cityTableWidget.setGeometry(QRect(280, 40, 331, 471))
+        self.settlementTableWidget = QTableWidget(Form)
+        self.settlementTableWidget.itemClicked.connect(lambda: self.fill_cities_table())
+        if (self.settlementTableWidget.columnCount() < 2):
+            self.settlementTableWidget.setColumnCount(2)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.settlementTableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem7)
+        __qtablewidgetitem8 = QTableWidgetItem()
+        self.settlementTableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem8)
+        self.settlementTableWidget.setObjectName(u"settlementTableWidget")
+        self.settlementTableWidget.setGeometry(QRect(20, 40, 231, 471))
+        self.label_3 = QLabel(Form)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(580, 40, 141, 16))
-        self.pushButton = QPushButton(Dialog)
+        self.label_3.setGeometry(QRect(660, 10, 101, 21))
+        font = QFont()
+        font.setPointSize(14)
+        self.label_3.setFont(font)
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(290, 10, 101, 21))
+        self.label_4.setFont(font)
+        self.label_5 = QLabel(Form)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(20, 10, 101, 21))
+        self.label_5.setFont(font)
+        self.pushButton = QPushButton(Form, clicked= lambda: self.edit_button_pressed() )
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(20, 580, 91, 31))
+        self.pushButton.setGeometry(QRect(30, 530, 181, 31))
 
-        self.retranslateUi(Dialog)
+        # hyeta dlya knopki vibora
+        self.settlementTableWidget.itemClicked.connect(self.set_edit_method(0))
+        self.cityTableWidget.itemClicked.connect(self.set_edit_method(1))
+        self.householdTableWidget.itemClicked.connect(self.set_edit_method(2))
+        # hyeta dlya knopki vibora
 
-        QMetaObject.connectSlotsByName(Dialog)
-        #loads the table
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+
         DB_PATH = 'MainDatabaseVet'  # bezvremennoe reshenie
         TABLE_ROW_LIMIT = 10
         vet_db_connection = create_engine(f'sqlite:///{DB_PATH}').connect()
@@ -155,19 +193,32 @@ class Ui_Dialog(object):
                 self.settlementTableWidget.setItem(row_num, col_num,
                                                    QTableWidgetItem(data_for_table.iloc[row_num, col_num]))
 
-
-
-
     # setupUi
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
-        self.label.setText(QCoreApplication.translate("Dialog", u"\u0420\u0430\u0439\u043e\u043d\u044b", None))
-        self.label_2.setText(QCoreApplication.translate("Dialog", u"\u0413\u043e\u0440\u043e\u0434\u0430", None))
-        self.settlementPushButton.setText(QCoreApplication.translate("Dialog", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u044b\u0431\u0440\u0430\u043d\u043d\u043e\u0435", None))
-        self.cityPushButton.setText(QCoreApplication.translate("Dialog", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u044b\u0431\u0440\u0430\u043d\u043d\u043e\u0435", None))
-        self.householdPushButton.setText(QCoreApplication.translate("Dialog", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u044b\u0431\u0440\u0430\u043d\u043d\u043e\u0435", None))
-        self.label_3.setText(QCoreApplication.translate("Dialog", u"\u0425\u043e\u0437\u044f\u0439\u0441\u0442\u0432\u0430", None))
-        self.pushButton.setText(QCoreApplication.translate("Dialog", u"PushButton", None))
+    def retranslateUi(self, Form):
+        Form.setWindowTitle("Правка Баз")
+
+        ___qtablewidgetitem = self.householdTableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"\u041f\u041a", None));
+        ___qtablewidgetitem1 = self.householdTableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"\u0410\u0434\u0440\u0435\u0441", None));
+        ___qtablewidgetitem2 = self.householdTableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"\u0412\u043b\u0430\u0434\u0435\u043b\u0435\u0446", None));
+        ___qtablewidgetitem3 = self.householdTableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"\u0413\u043e\u0440\u043e\u0434", None));
+        ___qtablewidgetitem4 = self.cityTableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("Form", u"\u041f\u041a", None));
+        ___qtablewidgetitem5 = self.cityTableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("Form", u"\u0413\u043e\u0440\u043e\u0434", None));
+        ___qtablewidgetitem6 = self.cityTableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("Form", u"\u0420\u0430\u0439\u043e\u043d", None));
+        ___qtablewidgetitem7 = self.settlementTableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("Form", u"\u041f\u041a", None));
+        ___qtablewidgetitem8 = self.settlementTableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem8.setText(QCoreApplication.translate("Form", u"\u0420\u0430\u0439\u043e\u043d", None));
+        self.label_3.setText(QCoreApplication.translate("Form", u"\u0425\u043e\u0437\u044f\u0439\u0441\u0442\u0432\u043e", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u0413\u043e\u0440\u043e\u0434", None))
+        self.label_5.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u0441\u0435\u043b\u0435\u043d\u0438\u0435", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"\u0420\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c \u0432\u044b\u0431\u0440\u0430\u043d\u043d\u043e\u0435", None))
     # retranslateUi
 
